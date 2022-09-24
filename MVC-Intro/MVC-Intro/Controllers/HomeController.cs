7a7ce58810c0c4280fd9baa2bee6415d275910ef
RadinTiholov <radin.tiholov@mail.bg>
2022-09-24 21:21:25 +0300

    Solve first problem

---
+++ MVC-Intro/MVC-Intro/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro.Models;
+using System.Diagnostics;
+
+namespace MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Some title here";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About() 
+        {
+            return View();
+        }
+        public IActionResult Numbers() 
+        {
+            return View();
+        }
+        public IActionResult NumbersGen(int count) 
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC-Intro/MVC-Intro/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro.Models;
+using System.Text;
+
+namespace MVC_Intro.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Egg",
+                Price = 300
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Milk",
+                Price = 200
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 12
+             }
+        };
+        public IActionResult All(string keyword)
+        {
+            if (keyword != null) 
+            {
+                var foundProducts = products
+                    .Where(x => x.Name.ToLower()
+                        .Contains(keyword.ToLower()));
+
+                return View(foundProducts);
+            }
+            return View(products);
+        }
+        public IActionResult AllAsJson() 
+        {
+            return Json(products);
+        }
+        public IActionResult AllAsText()
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var product in products)
+            {
+                result.Append($"Product {product.Id} : {product.Name} - {product.Price}$\n");
+            }
+            return Content(result.ToString());
+        }
+        public IActionResult ById(int id) 
+        {
+            var product = this.products.FirstOrDefault(x => x.Id == id);
+            if (product == null) 
+            {
+                return BadRequest();
+            }
+            return View(product);
+        }
+    }
+}
