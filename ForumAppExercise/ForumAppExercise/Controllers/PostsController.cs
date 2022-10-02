9ae907971b5c6edd884e5fa1c8814eb3269f6a1f
RadinTiholov <radin.tiholov@mail.bg>
2022-10-02 17:32:52 +0300

    Add posts controller

---
+++ ForumAppExercise/ForumAppExercise/Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using ForumAppExercise.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumAppExercise.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly ForumAppDbContext dbContext;
+        public PostsController(ForumAppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
