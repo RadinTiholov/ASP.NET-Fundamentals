2f832ea789acd86ecff31d1743812336bee11564
RadinTiholov <radin.tiholov@mail.bg>
2022-10-02 17:12:53 +0300

    Initialize, add Post model and add DbContext

---
+++ ForumAppExercise/ForumAppExercise/Data/ForumAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static ForumAppExercise.Data.DataConstants;
+
+namespace ForumAppExercise.Data
+{
+    public class ForumAppDbContext : DbContext
+    {
+        public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ ForumAppExercise/ForumAppExercise/Data/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ForumAppExercise.Data.DataConstants.Post;
+
+namespace ForumAppExercise.Data.Models
+{
+    public class Post
+    {
+        public Guid Id { get; init; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        public string Content { get; set; }
+    }
+}
