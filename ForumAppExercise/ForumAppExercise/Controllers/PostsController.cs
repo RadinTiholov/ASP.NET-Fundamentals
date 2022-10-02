2aae22fa58f397151e0bdd849fe0ee2226167a8f
RadinTiholov <radin.tiholov@mail.bg>
2022-10-02 18:09:04 +0300

    Add get and post actions for adding a new post

--- ForumAppExercise/ForumAppExercise/Controllers/PostsController.cs
+++ ForumAppExercise/ForumAppExercise/Controllers/PostsController.cs
@@ -1,6 +1,9 @@
 ﻿using ForumAppExercise.Data;
+using ForumAppExercise.Data.Models;
+using ForumAppExercise.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace ForumAppExercise.Controllers
 {
@@ -12,9 +15,40 @@ namespace ForumAppExercise.Controllers
             this.dbContext = dbContext;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            var posts = dbContext
+                .Posts
+                .Select(x => new PostViewModel 
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Content = x.Content
+                })
+                .ToList();
+
+            return View(posts);
+        }
+        [HttpGet]
+        public IActionResult Add() 
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(PostFormModel model) 
+        {
+            var post = new Post() 
+            {
+                Title = model.Title,
+                Content = model.Content
+            };
+
+            this.dbContext.Add(post);
+            this.dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
