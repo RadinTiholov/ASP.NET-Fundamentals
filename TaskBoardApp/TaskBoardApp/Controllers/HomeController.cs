dd8892f7a605ccd4bd659214b8e1569db34cb132
RadinTiholov <radin.tiholov@mail.bg>
2022-10-18 21:37:58 +0300

    Create home page controller and view

--- TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
+++ TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -1,14 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
+using TaskBoardApp.Data;
 using TaskBoardApp.Models;
 
 namespace TaskBoardApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly TaskBoardAppDbContext dbContext;
+
+        public HomeController(TaskBoardAppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var taskBoards = dbContext
+                .Boards
+                .Select(b => b.Name)
+                .Distinct();
+
+            var taskCounts = new List<HomeBoardModel>();
+            foreach (var boardName in taskBoards) 
+            {
+                var tasksInBoard = dbContext.Task.Where(t => t.Board.Name == boardName).Count();
+                taskCounts.Add(new HomeBoardModel()
+                {
+                    BoardName = boardName,
+                    TasksCount = tasksInBoard
+                });
+            }
+
+            var userTaskCount = -1;
+
+            if (User.Identity.IsAuthenticated) 
+            {
+                var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                userTaskCount = dbContext.Task.Where(x => x.OwnerId == currentUserId).Count();
+            }
+
+            var homeModel = new HomeViewModel() 
+            {
+                AllTasksCount = dbContext.Task.Count(),
+                BoardsWithTasksCount = taskCounts,
+                UserTasksCount = userTaskCount
+            };
+
+            return View(homeModel);
         }
     }
 }
---
+++ TaskBoardApp/TaskBoardApp/Models/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskBoardApp.Models
+{
+    public class HomeViewModel
+    {
+        public int AllTasksCount { get; set; }
+        public int UserTasksCount { get; set; }
+        public IList<HomeBoardModel> BoardsWithTasksCount { get; set; }
+    }
+}
